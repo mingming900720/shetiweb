d9f2c3d81c62bc9bc9920ea8162827da5058601b
mingming900720 <zzugm@outlook.com>
2014-10-30 17:08:28 +0800

    upload *.revm test

--- upload.php
+++ upload.php
@@ -1,10 +1,14 @@
 <?php
-		require "FileUpload.class.php";
+	require "FileUpload.class.php";
+
+	$up=new FileUpload(array('isRandName'=>true,'allowType'=>array('txt', 'doc', 'mp3', 'jpg', 'gif', 'rmvb'),'FilePath'=>'/home/wwwroot/default/shangchuan/uploads/', 'MAXSIZE'=>2000000000));
+
+	echo '<pre>';
+	if($up->uploadFile('spic')){
+		print_r($up->getNewFileName());
+	}else{
+		print_r($up->getErrorMsg());	
+	}
+	
+	echo '</pre>';
 		
-		$up=new FileUpload();
-		
-		if($up->uploadFile('spic')){
-			$up->echo $up->getNewFileName();
-		}else{
-			echo $up->getErrorMsg();
-		}
